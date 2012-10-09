bb09f0468ad42d405aa0543143a4dc036ff0fea4
norberto <norberto.delateja@gmail.com>
2012-10-08 19:01:49 -0500

    Comit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CUCUMBERExample::Application.config.secret_token = '2694aaad34e9c822d744da1b2820f56ad45e486b2aa42b50840e9bb039abc02dd31ac425ff4f36ba1d7b0ba7cbef75ae6dd34f139398368ebde81aa8f26b42b9'
